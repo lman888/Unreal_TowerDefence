3ace07ec6cd6bd291f35e2b4f9c2bdb29c1cd302
liam whitehouse <liam.whitehouse@bigant.com>
2025-07-23 15:56:12 +1000

    Starting to add in tower upgrade click events.

--- TowerDefence/Source/TowerDefence/Public/Towers/MasterTower.cpp
+++ TowerDefence/Source/TowerDefence/Public/Towers/MasterTower.cpp
@@ -12,6 +12,8 @@ AMasterTower::AMasterTower()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+
+	bReplicates = true;
 	
 	AbilitySystemComponent = CreateDefaultSubobject<UTDAbilitySystemComponent>("AbilitySystemComponent");
 	AbilitySystemComponent->SetIsReplicated(true);
