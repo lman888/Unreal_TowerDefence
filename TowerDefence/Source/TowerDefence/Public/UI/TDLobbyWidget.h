3240a506464fea73f474aa29a01c0ae1e165497e
liam whitehouse <liam.whitehouse@bigant.com>
2025-10-21 15:56:18 +1100

    Starting the Lobby UI Creation

---
+++ TowerDefence/Source/TowerDefence/Public/UI/TDLobbyWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "TDLobbyWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TOWERDEFENCE_API UTDLobbyWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
---
+++ TowerDefence/Source/TowerDefence/Public/UI/TDMainMenu.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "TDMainMenu.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TOWERDEFENCE_API UTDMainMenu : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
